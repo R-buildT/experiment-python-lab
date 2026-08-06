e93af54df9302fa2191cdf743b63b19eac4d3a2d
R.buildT <RT.builds@proton.me>
2026-08-06 09:58:41 +0300

    modified variables

--- week 3/calculatorprototype.py
+++ week 3/calculatorprototype.py
@@ -1,46 +1,47 @@
-a = input("Enter a number: ")
-b = input("Enter another number: ")
-c = input("Enter a third number: ")
-d = input("enter operation: ")
-def func(a,b,c):
+num1_str = input("Enter a number: ")
+num2_str = input("Enter another number: ")
+num3_str = input("Enter a third number: ")
+operation = input("enter operation: ")
+
+def calculate(num1_str, num2_str, num3_str, operation):
     try:
-        if d == '+':
-            if c == '':
-                return int(a) + int(b)
-            elif b == '':
-                return int(a) + int(c)
-            elif a == '':
-                return int(b) + int(c)
+        if operation == '+':
+            if num3_str == '':
+                return int(num1_str) + int(num2_str)
+            elif num2_str == '':
+                return int(num1_str) + int(num3_str)
+            elif num1_str == '':
+                return int(num2_str) + int(num3_str)
             else:
-                return int(a) + int(b) + int(c)
-        elif d == '-':
-            if c == '':
-                return int(a) - int(b)
-            elif b == '':
-                return int(a) - int(c)
-            elif a == '':
-                return int(b) - int(c)
+                return int(num1_str) + int(num2_str) + int(num3_str)
+        elif operation == '-':
+            if num3_str == '':
+                return int(num1_str) - int(num2_str)
+            elif num2_str == '':
+                return int(num1_str) - int(num3_str)
+            elif num1_str == '':
+                return int(num2_str) - int(num3_str)
             else:
-                return int(a) - int(b) - int(c)
-        elif d == '*':
-            if c == '':
-                return int(a) * int(b)
-            elif b == '':
-                return int(a) * int(c)
-            elif a == '':
-                return int(b) * int(c)
+                return int(num1_str) - int(num2_str) - int(num3_str)
+        elif operation == '*':
+            if num3_str == '':
+                return int(num1_str) * int(num2_str)
+            elif num2_str == '':
+                return int(num1_str) * int(num3_str)
+            elif num1_str == '':
+                return int(num2_str) * int(num3_str)
             else:
-                return int(a) * int(b) * int(c)
-        elif d == ('/'):
-            if c == '':
-                return int(a)/int(b)
-            elif b == '':
-                return int(a)/int(c)
-            elif a == '':
-                return int(b)/int(c)
+                return int(num1_str) * int(num2_str) * int(num3_str)
+        elif operation == '/':
+            if num3_str == '':
+                return int(num1_str) / int(num2_str)
+            elif num2_str == '':
+                return int(num1_str) / int(num3_str)
+            elif num1_str == '':
+                return int(num2_str) / int(num3_str)
         else:
-            return('invalid operation')
+            return 'invalid operation'
     except ValueError:
         return "invalid value"
 
-print(func(a,b,c))
+print(calculate(num1_str, num2_str, num3_str, operation))
